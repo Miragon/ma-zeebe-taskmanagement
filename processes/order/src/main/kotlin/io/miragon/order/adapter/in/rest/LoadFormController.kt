a96757c23804e59833b7224277b877c3bab7ec75
peterhnm <peter.heinemann@miragon.io>
2024-06-16 09:29:31 +0200

    feat(stack): allow cross origin requests

--- processes/order/src/main/kotlin/io/miragon/order/adapter/in/rest/LoadFormController.kt
+++ processes/order/src/main/kotlin/io/miragon/order/adapter/in/rest/LoadFormController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/rest/task/load")
+@RequestMapping("/rest/task")
 class LoadFormController(
     private val loadCheckOrderFormUseCase: LoadFormUseCase.LoadCheckOrderFormUseCase,
     private val loadPrepareOrderFormUseCase: LoadFormUseCase.LoadPrepareOrderFormUseCase,
--- taskmanager/src/main/kotlin/io/miragon/taskmanager/adapter/in/rest/LoadUserTaskController.kt
+++ taskmanager/src/main/kotlin/io/miragon/taskmanager/adapter/in/rest/LoadUserTaskController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/rest/load")
+@RequestMapping("/rest")
 class LoadUserTaskController(
     private val loadUserTaskUseCase: LoadUserTaskUseCase
 )
