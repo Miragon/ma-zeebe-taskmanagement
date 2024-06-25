4be9c9e5c5682190203a148a55b0a0bcf7d25430
peterhnm <peter.hnm@gmx.de>
2024-06-25 14:32:00 +0200

    feat(nginx): add broadcasting functionality to nginx

---
+++ stack/nginx/njs/broadcast.js
@@ -0,0 +1,16 @@
+async function join(r) {
+    let uris = [
+        "/orderProcess/rest/metadata"
+    ]
+    join_subrequests(r, uris)
+}
+
+async function join_subrequests(r, subs) {
+    let results = await Promise.all(subs.map(uri => r.subrequest(uri)));
+
+    let response = results.map(reply => JSON.parse(reply.responseText));
+
+    r.return(200, JSON.stringify(response));
+}
+
+export default {join};
