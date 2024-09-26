9096c14052c324fd87a61de39cf4bc054b468e04
peterhnm <peter.heinemann@miragon.io>
2024-09-26 12:08:42 +0200

    feat: update bpmn diagrams

--- taskmanager/src/main/kotlin/io/miragon/zeebe/taskmanager/adapter/out/database/ProcessVariableEntity.kt
+++ taskmanager/src/main/kotlin/io/miragon/zeebe/taskmanager/adapter/out/database/ProcessVariableEntity.kt
@@ -2,8 +2,11 @@ package io.miragon.zeebe.taskmanager.adapter.out.database
 
 import io.miragon.zeebe.taskmanager.domain.ProcessVariable
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import java.util.UUID
 
 @Entity
 @Table(name = "process_variable")
@@ -26,9 +29,11 @@ class ProcessVariableEntity(
     fun toDomain(): ProcessVariable
     {
         return ProcessVariable(
-            processInstanceKey = id,
+            processInstanceKey = processInstanceKey,
             name = name,
             value = value,
+            bpmnProcessId = null,
+            processDefinitionKey = null,
         )
     }
 }
