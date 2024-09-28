b2dd4a3d970b9330a5dacf523f80a9b477692479
peterhnm <peter.heinemann@miragon.io>
2024-09-28 17:29:12 +0200

    chore: use job worker

--- zeebe-user-task-exporter/src/main/kotlin/io/miragon/zeebe/tm/exporter/RecordFilter.kt
+++ zeebe-user-task-exporter/src/main/kotlin/io/miragon/zeebe/tm/exporter/RecordFilter.kt
@@ -6,18 +6,11 @@ import io.camunda.zeebe.protocol.record.ValueType
 
 class RecordFilter : Context.RecordFilter
 {
-    /**
-     * Accepts only Event records
-     */
     override fun acceptType(recordType: RecordType?): Boolean
     {
         return recordType == RecordType.EVENT
     }
 
-    /**
-     * Accepts only Job and User Task records
-     * A Job record is created when a user task is created, timed out or canceled
-     */
     override fun acceptValue(valueType: ValueType?): Boolean
     {
         return valueType == ValueType.USER_TASK || valueType == ValueType.VARIABLE
